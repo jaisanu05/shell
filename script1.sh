fc32d57d4655ead5d2a86f15e5a2f2083d0b404e
Anupama <anupamakp23@gmail.com>
2024-06-23 05:56:06 +0000

    Changes in 2% tax

--- script1.sh
+++ script1.sh
@@ -2,12 +2,16 @@
 read -p "Enter Your Salary " s
 a="5/100"
 b="10/100"
+c="2/100"
 
 if [ $s -lt 20000 ]
 then
 	echo "No tax"
+elif [ $s == 20000 ]
+then
+	        echo 2% Tax = $(($s - $s*$c))
 
-elif [ $s -ge 20000 -a $s -le 40000 ]
+elif [ $s -gt 20000 -a $s -le 40000 ]
 then
 	echo 5% Tax = $(($s - $s*$a))
 
