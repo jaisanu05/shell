e65de386cd89a4af8d864627d55e87bd10f9ea11
Anupama <anupamakp23@gmail.com>
2024-06-23 06:18:16 +0000

    new file

---
+++ task.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+read -p "Enter Number one " o
+read -p "Enter Number two " t
+
+echo Addition = $(( $o + $t ))
+echo Subtraction = $(( $o - $t ))
+echo Multiply = $(( $o * $t ))
+echo Division = $(( $o / $t ))
+
+read -p "Enter Your Salary " s
+a="5/100"
+b="10/100"
+c="2/100"
+
+if [ $s -lt 20000 ]
+then
+	echo "No tax"
+elif [ $s == 20000 ]
+then
+	echo 2% Tax = $(($s - $s*$c))
+
+elif [ $s -gt 20000 -a $s -le 40000 ]
+then
+	echo 5% Tax = $(($s - $s*$a))
+
+elif [ $s -gt 40000 ]
+then
+	echo 10% Tax = $(($s - $s*$b))
+fi
+
+
+read -p "Enter Your Age " d
+
+if [ $d -lt 13 ]
+then
+	echo "Child"
+
+elif [ $d -ge 13 -a $d -le 19 ]
+then
+	echo "teenager"
+elif [ $d -ge 20 -a $d -le 60 ]
+then
+	echo "Adult"
+elif [ $d -ge 60 ]
+then
+	echo "Oldage"
+fi
